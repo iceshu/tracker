c22f419105be0978249497902488f5769b56c318
pc <pc@pc.com>
2024-04-01 08:07:49 +0800

    增加env配置

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,19 @@ import json from "@rollup/plugin-json";
 import pkg from "./package.json" assert { type: "json" };
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+import { uglify } from "rollup-plugin-uglify";
+import { terser } from "rollup-plugin-terser";
+const name = "__TC_TRACKER__";
+const isProduction = process.env.NODE_ENV === "production";
+
+const plugins = [
+  typescript({ outDir: "dist", declaration: true, declarationDir: "dist" }),
+  json(),
+  resolve(),
+  commonjs(),
+  isProduction && uglify(),
+  isProduction && terser(),
+].filter(Boolean);
 
 export default {
   input: "./src/index.ts",
@@ -10,17 +23,21 @@ export default {
     {
       format: "cjs",
       file: pkg.main,
+      exports: "named",
+    },
+    {
+      format: "umd",
+      file: pkg.umd,
+      name,
+      exports: "named",
     },
     {
       format: "es",
       file: pkg.module,
       name: pkg.name,
+      exports: "named",
     },
   ],
-  plugins: [
-    typescript({ outDir: "dist", declaration: true, declarationDir: "dist" }),
-    json(),
-    resolve(),
-    commonjs(),
-  ],
+  plugins: plugins,
 };
+console.log("111111111", process.env.NODE_ENV);
